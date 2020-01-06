2056047879dbd1a37cbd105dab723adb04bdcf64
binkynz <46007722+binkynz@users.noreply.github.com>
2020-01-06 15:18:31 +1300

    Add files via upload
    shorter sigs

--- engine_prediction.cpp
+++ engine_prediction.cpp
@@ -116,11 +116,11 @@ namespace n_engine_prediction {
 
 					n_utilities::get_virtual_function< void( __thiscall* )( void* ) >( player, 219 )( player ); // StudioFrameAdvance( )
 
-					static auto post_think_v_physics = reinterpret_cast< bool( __thiscall* )( C_BaseEntity* ) >( n_utilities::pattern_scan( "client_panorama.dll", "55 8B EC 83 E4 F8 81 EC ? ? ? ? 53 8B D9 56 57 83 BB ? ? ? ? ? 75 50" ) );
+					static auto post_think_v_physics = reinterpret_cast< bool( __thiscall* )( C_BaseEntity* ) >( n_utilities::pattern_scan( "client_panorama.dll", "55 8B EC 83 E4 F8 81 EC ? ? ? ? 53 8B D9 56 57 83 BB" ) );
 					post_think_v_physics( player );
 				}
 
-				static auto simulate_player_simulated_entities = reinterpret_cast< bool( __thiscall* )( C_BaseEntity* ) >( n_utilities::pattern_scan( "client_panorama.dll", "56 8B F1 57 8B BE ? ? ? ? 83 EF 01 78 72 90 8B 86" ) );
+				static auto simulate_player_simulated_entities = reinterpret_cast< bool( __thiscall* )( C_BaseEntity* ) >( n_utilities::pattern_scan( "client_panorama.dll", "56 8B F1 57 8B BE ? ? ? ? 83 EF 01 78 72" ) );
 				simulate_player_simulated_entities( player );
 
 				return n_utilities::get_virtual_function< int( __thiscall* )( void* ) >( n_interfaces::model_cache, 34 )( n_interfaces::model_cache );
